cd3e9949a0c18b252c0fb085c352dc6f58222459
KimGo1 <Kimberlygoods36@gmail.com>
2023-11-22 00:01:44 -0600

    final

--- app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactFormActivity.java
@@ -1,9 +1,101 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 import androidx.appcompat.app.AppCompatActivity;
+import edu.ualr.intentsassignment.databinding.ActivityContactFormBinding;
+import edu.ualr.intentsassignment.model.Contact;
 
 public class ContactFormActivity extends AppCompatActivity {
+
+    private ActivityContactFormBinding mBinding;
+    private EditText editTextName;
+    private EditText editTextEmail;
+    private String phone;
+    private String address;
+    private String website;
+    private String email;
+    private String firstName;
+    private String lastName;
     // TODO 01. Create a new layout file to define the GUI elements of the ContactFormActivity.
     // TODO 02. Define the basic skeleton of the ContactFormActivity. Inflate the layout defined in the first step to display the GUI elements on screen.
-    // TODO 06. Create a new method that reads the values in the several EditText elements of the layout and then uses the Contact class to send those data to ContactInfoActivity
-}
+    // TODO 06. Create a new method that reads the values in the several Edi
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinding = ActivityContactFormBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+
+
+        //Initialize EditText and buttonSubmit fields
+        EditText editTextFirstName = findViewById(R.id.editTextFirstName);
+        EditText editTextLastName = findViewById(R.id.editTextLastName);
+        EditText editTextPhoneNumber = findViewById(R.id.editTextPhoneNumber);
+        EditText editTextEmail = findViewById(R.id.editTextEmail);
+        EditText editTextAddress = findViewById(R.id.editTextAddress);
+        EditText editTextWebsite = findViewById(R.id.editTextWebsite);
+        Button buttonSubmit = findViewById(R.id.buttonSubmit);
+
+        buttonSubmit.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                //Read data from EditText fields
+                String firstName = editTextFirstName.getText().toString();
+                String lastName = editTextLastName.getText().toString();
+                String phone = editTextPhoneNumber.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String address = editTextAddress.getText().toString();
+                String website = editTextWebsite.getText().toString();
+
+                //Create a Contact instance with the read data
+                Contact contact = new Contact();
+                contact.setFirstName(firstName);
+                contact.setLastName(lastName);
+                contact.setPhoneNumber(phone);
+                contact.setEmailAddress(email);
+                contact.setAddress(address);
+                contact.setWebsite(website);
+
+                //Method to send data to ContactInfoActivity is Called
+                sendDataToContactInfo(contact);
+            }
+        });
+    }
+
+
+    private void dialPhoneNumber(String phoneNumber) {
+        if (phoneNumber != null && !phoneNumber.isEmpty()) {
+            Intent dialIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phoneNumber));
+            startActivity(dialIntent);
+        }
+    }
+
+    private void displayContactInfo(Contact contact) {
+    }
+
+    //Send the data to ContactInfoActivity
+    private void sendDataToContactInfo(Contact contact) {
+                Intent intent;
+                intent = new Intent(ContactFormActivity.this, ContactInfoActivity.class);
+                intent.putExtra("contact", contact);
+                startActivity(intent);
+
+            }
+
+    }
+
+
+
+
--- app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
+++ app/src/main/java/edu/ualr/intentsassignment/ContactInfoActivity.java
@@ -1,6 +1,19 @@
 package edu.ualr.intentsassignment;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.WindowDecorActionBar;
+
+
+import java.text.BreakIterator;
+
+import edu.ualr.intentsassignment.databinding.ActivityContactInfoBinding;
+import edu.ualr.intentsassignment.model.Contact;
 
 public class ContactInfoActivity extends AppCompatActivity {
     // TODO 03. Create a new layout file to define the GUI elements of the ContactInfoActivity.
@@ -11,4 +24,120 @@ public class ContactInfoActivity extends AppCompatActivity {
     // TODO 10. Create a new method that invokes a Maps app, using as parameter the address included in the contact info received from ContactFormActivity in the 7th step
     // TODO 11. Create a new method that invokes an Email app, using as parameter the email address included in the contact info received from ContactFormActivity in the 7th step
     // TODO 12. Create a new method that invokes an Web Browser app, using as parameter the web url included in the contact info received from ContactFormActivity in the 7th step
+    private ActivityContactInfoBinding mBinding;
+    private BreakIterator textViewName;
+    private BreakIterator textViewEmail;
+    private Object contact;
+    private WindowDecorActionBar.TabImpl textViewContactInfo;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        mBinding = ActivityContactInfoBinding.inflate(getLayoutInflater());
+        View view = mBinding.getRoot();
+        setContentView(view);
+
+
+        View textViewContactInfo = findViewById(R.id.textViewContactName);
+
+
+        // Retrieve the Contact data from the intent
+        Intent intent = getIntent();
+        if (intent != null) {
+            Contact contact = intent.getParcelableExtra("contact");
+            //To display  the contact information
+            displayContactInfo(contact);
+        }
+
+
+
+        //To invoke the Message app with the phone number
+        if (contact != null) {
+           // sendSmsMessage(contact.getPhoneNumber());
+        }
+        //To invoke the Maps app with the address
+        if (contact != null) {
+           // openMapsApp(contact.getAddress());
+        }
+
+        //To invoke the Email app with the email address
+        if (contact != null) {
+           // sendEmail(contact.getEmailAddress());
+        }
+
+        //To invoke the Web Browser app with the web URL
+        if (contact != null) {
+        //    openWebBrowser(contact.getWebsite());
+        }
+
+
+        }
+
+
+    private void dialPhoneNumber(String phoneNumber) {
+    }
+
+    private void openWebBrowser(String website) {
+        //To invoke the Web Browser app
+        if (website != null && !website.isEmpty()) {
+            //Create an intent to open the Email app with the email address provided
+            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(website));
+
+            //Check for Web Browser app
+            if (webIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(webIntent);
+            }
+        }
+    }
+
+    private void sendEmail(String emailAddress) {
+        //To invoke the Email app
+        if (emailAddress != null && !emailAddress.isEmpty()) {
+            //Create an intent to open the Email app with the email address provided
+            Intent emailIntent = new Intent(Intent.ACTION_SENDTO);
+            emailIntent.setData(Uri.parse("mailto:" + emailAddress));
+
+            //Check for Maps app available on the device
+            if (emailIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(emailIntent);
+            }
+        }
+     }
+
+    private void openMapsApp(String address) {
+        if (address != null && !address.isEmpty()) {
+            //To encode the address to create a URI for the Maps app
+            Uri locationUri = Uri.parse("geo:0,0?q=" + Uri.encode(address));
+
+            //To create an intent to open the Maps app with the location
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, locationUri);
+
+            //Check for Maps app available on the device
+            if (mapIntent.resolveActivity(getPackageManager()) != null) {
+                startActivity(mapIntent);
+            }
+
+        }
+     }
+
+
+    private void displayContactInfo(Contact contact) {
+        if (contact != null) {
+            mBinding.textViewContactName.setText(contact.getFullName());
+            mBinding.textPhoneNumber.setText(contact.getPhoneNumber());
+            mBinding.textViewEmail.setText(contact.getEmailAddress());
+            mBinding.textViewAddress.setText(contact.getAddress());
+            mBinding.textViewWebsite.setText(contact.getWebsite());
+
+        }
+    }
+    private void sendSmsMessage(String phoneNumber) {
+
+        if(phoneNumber != null && !phoneNumber.isEmpty()) {
+            Intent smsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("sms:" + phoneNumber));
+            startActivity(smsIntent);
+        }
+    }
+
 }
